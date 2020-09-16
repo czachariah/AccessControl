c3b9d07fb1c40111ec1230d65c3a68ebfc7db11e
czachariah <zachariahchris@yahoo.com>
2020-09-16 11:46:18 -0400

    initial code setup completed

---
+++ portal.py
@@ -0,0 +1,21 @@
+"""
+Access Control Program
+
+CS419: Computer Security
+Project 1
+
+Author: Chris Zachariah (cvz2)
+"""
+import sys
+import os
+
+
+# main method for the program
+def main():
+    print("started")
+
+
+# runs the program by calling the main() method
+if __name__ == "__main__":
+    main()
+
