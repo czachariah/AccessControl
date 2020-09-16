3ba83a9fa680a934c60a56fcf43325f8eeb1965e
czachariah <zachariahchris@yahoo.com>
2020-09-16 12:19:01 -0400

    proper set up to take in command complete

--- portal.py
+++ portal.py
@@ -12,7 +12,39 @@ import os
 
 # main method for the program
 def main():
-    print("started")
+    # make sure that the correct number of arguments are given
+    if len(sys.argv) < 2:
+        print("\nSTATUS: ERROR! Please include a command and the proper arguments.\n")
+        exit()
+
+    # make sure the command given is valid
+    if sys.argv[1] == "AddUser":
+        print("AddUser Command!")
+
+    elif sys.argv[1] == "Authenticate":
+        print("Authenticate Command!")
+
+    elif sys.argv[1] == "SetDomain":
+        print("SetDomain Command!")
+
+    elif sys.argv[1] == "DomainInfo":
+        print("DomainInfo Command!")
+
+    elif sys.argv[1] == "SetType":
+        print("SetType Command!")
+
+    elif sys.argv[1] == "TypeInfo":
+        print("TypeInfo Command!")
+
+    elif sys.argv[1] == "AddAccess":
+        print("AddAccess Command!")
+
+    elif sys.argv[1] == "CanAccess":
+        print("CanAccess Command!")
+
+    else:
+        print("\nSTATUS: ERROR! Please use a proper command.\n")
+        exit()
 
 
 # runs the program by calling the main() method
